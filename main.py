363daa7944a4d0c590ccdf4c3705a41cb1b960a0
Daniel Agapov <danielagapov1@gmail.com>
2021-07-18 05:19:18 -0400

    setting background working

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os, sys, ctypes
+
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk, filedialog
 from PIL import ImageTk, Image
+
 
 HEIGHT, WIDTH = 768, 1366
 
@@ -15,30 +18,8 @@ def main():
     
     # #overall GUI loop which will run constantly, accepting input and such
     root.mainloop()    
-def retrieve_file(lower_frame):
-    a = 'compatible image files'
 
-    name_of_file = filedialog.askopenfilename(initialdir='/This PC', title='Select an Image File', filetypes=(
-        (a, "*.png"), 
-        (a, "*.jpeg"), 
-        (a, "*.jpg*"), #should be *.jpg
-        (a, "*.gif"),
-        (a, "*.tiff"),
-        (a, "*.psd"),
-        (a, "*.pdf"),
-        (a, "*.eps"),
-        (a, "*.ai"),
-        (a, "*.indd"),
-        (a, "*.raw")
-    )
-    )
 
-    background_uploaded = ImageTk.PhotoImage(Image.open(name_of_file))
-    background_uploaded_label = tk.Label(lower_frame) 
-    background_uploaded_label.place(relheight=1, relwidth=1)
-    background_uploaded_label.configure(image=background_uploaded)
-    background_uploaded_label.image = background_uploaded
-    print(name_of_file)
 
 class main_screen():
     def __init__(self, master):
@@ -62,7 +43,7 @@ class main_screen():
         #I only want its command to run once, when it's clicked so I made a 
         #simple lambda that invokes the info_display function
         self.button = tk.Button(self.weather_frame, text="Select Image in File Explorer", font=('Courier', 40), bg='white', 
-        command=lambda: retrieve_file(self.lower_frame))
+        command=lambda: main_screen.retrieve_file(self.lower_frame))
         self.button.place(relx=0, relheight=1, relwidth=1)
 
         #submit button to accept the file
@@ -73,9 +54,44 @@ class main_screen():
         self.submit_pic = tk.PhotoImage(file='./images/submit_pic.png')
         self.submit_pic_new = self.submit_pic.subsample(2, 2)
 
-        self.submit_pic_button = tk.Button(self.submit_frame, image=self.submit_pic_new, bg="white")#command=lambda:
+        self.submit_pic_button = tk.Button(self.submit_frame, image=self.submit_pic_new, bg="white", 
+        command=lambda:main_screen.set_background_uploaded(self))
         self.submit_pic_button.place(relx=0, relheight=1, relwidth=1) 
     
+    def retrieve_file(lower_frame):
+        a = 'compatible image files'
+
+
+        global name_of_file
+
+        name_of_file = filedialog.askopenfilename(initialdir='/This PC', title='Select an Image File', filetypes=(
+            (a, "*.png"), 
+            (a, "*.jpeg"), 
+            (a, "*.jpg*"), #should be *.jpg
+            (a, "*.gif"),
+            (a, "*.tiff"),
+            (a, "*.psd"),
+            (a, "*.pdf"),
+            (a, "*.eps"),
+            (a, "*.ai"),
+            (a, "*.indd"),
+            (a, "*.raw")
+        )
+        )
+
+        background_uploaded = ImageTk.PhotoImage(Image.open(name_of_file))
+        
+        background_uploaded_label = tk.Label(lower_frame) 
+        background_uploaded_label.place(relheight=1, relwidth=1)
+
+        background_uploaded_label.configure(image=background_uploaded)
+        background_uploaded_label.image = background_uploaded
+    
+    
+    def set_background_uploaded(self):
+        self.path = os.path.abspath(os.path.dirname(sys.argv[0]))
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, name_of_file , 0)
+
 
 if __name__ == '__main__':
     main()
