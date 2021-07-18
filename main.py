205d76db4baa937dd700ba8a174b8a9b4fffaebb
Daniel Agapov <danielagapov1@gmail.com>
2021-07-17 22:14:43 -0400

    got the file, couldn't display it though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
+from PIL import ImageTk, Image
 
 HEIGHT = 768
 WIDTH = 1366
@@ -13,26 +14,29 @@ def main():
     app = main_screen(root)
     
     # #overall GUI loop which will run constantly, accepting input and such
-    root.mainloop()
-
-def retrieve_file():
+    root.mainloop()    
+def retrieve_file(lower_frame):
     a = 'compatible image files'
-    root.filename = filedialog.askopenfilename(initialdir='/', title='Select an Image File', filetypes=(
-        ("compatible image files", "*.png"), 
-        (a, "*.jpeg"), 
-        (a, "*.jpg"),
-        (a, "*.gif"),
-        (a, "*.tiff"),
-        (a, "*.psd"),
-        (a, "*.pdf"),
-        (a, "*.eps"),
-        (a, "*.ai"),
-        (a, "*.indd"),
-        (a, "*.raw")
-    )
+
+    name_of_file = filedialog.askopenfilename(initialdir='/This PC', title='Select an Image File', filetypes=(
+            # (a", "*.png"), 
+            # (a, "*.jpeg"), 
+            (a, "*.*"), #should be *.jpg
+            # (a, "*.gif"),
+            # (a, "*.tiff"),
+            # (a, "*.psd"),
+            # (a, "*.pdf"),
+            # (a, "*.eps"),
+            # (a, "*.ai"),
+            # (a, "*.indd"),
+            # (a, "*.raw")
+        )
     )
 
-    
+    background_uploaded = ImageTk.PhotoImage(Image.open(name_of_file))
+    background_uploaded_label = tk.Label(lower_frame, image=background_uploaded)
+    background_uploaded_label.pack()
+    print(name_of_file)
 
 class main_screen():
     def __init__(self, master):
@@ -56,7 +60,7 @@ class main_screen():
         #I only want its command to run once, when it's clicked so I made a 
         #simple lambda that invokes the info_display function
         self.button = tk.Button(self.weather_frame, text="Select Image in File Explorer", font=('Courier', 40), bg='white', 
-        command=lambda:retrieve_file())
+        command=lambda: retrieve_file(self.lower_frame))
         self.button.place(relx=0, relheight=1, relwidth=1)
 
         #submit button to accept the file
@@ -70,6 +74,7 @@ class main_screen():
         self.submit_pic_button = tk.Button(self.submit_frame, image=self.submit_pic_new, bd=5
         , bg="white")#command=lambda:
         self.submit_pic_button.place(relx=0, relheight=1, relwidth=1) 
+    
 
 if __name__ == '__main__':
     main()
